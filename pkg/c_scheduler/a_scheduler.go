b0eb59efeaf5412905c987ff540d708f91ea1e0a
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-12-07 13:14:44 -0800

    txn

--- pkg/c_scheduler/a_scheduler.go
+++ pkg/c_scheduler/a_scheduler.go
@@ -4,34 +4,34 @@ import (
 	"sync/atomic"
 	"time"
 	"tiny_txn/pkg/a_misc/errmsg"
-	tsmarker "tiny_txn/pkg/d_ts_marker"
+	tsmarker "tiny_txn/pkg/d_ts_waiter"
 	cache "tiny_txn/pkg/f_cache"
 	checkpoint "tiny_txn/pkg/g_checkpoint"
 	wal "tiny_txn/pkg/h_wal"
 )
 
-type TxnScheduler struct {
+type TsoScheduler struct {
 	ts    uint64
 	minTs uint64 // min ts
 
-	beginTsMarker  tsmarker.BeginTsMarker
-	commitTsMarker tsmarker.CommitTsMarker
+	beginTsWaiter  tsmarker.BeginTsMarker
+	commitTsWaiter tsmarker.CommitTsMarker
 
 	stopCh       chan struct{} // for halting until the current transaction is done.
 	reqCh        chan *request
 	checkPointer checkpoint.CheckPointer
 }
 
-var _ Scheduler = &TxnScheduler{}
+var _ Scheduler = &TsoScheduler{}
 
 func New(ts uint64, w wal.Wal) Scheduler {
-	return &TxnScheduler{
+	return &TsoScheduler{
 		ts:    ts,
 		minTs: ts,
 
 		stopCh:         make(chan struct{}),
-		beginTsMarker:  tsmarker.NewBegin(),
-		commitTsMarker: tsmarker.NewCommit(),
+		beginTsWaiter:  tsmarker.NewBegin(),
+		commitTsWaiter: tsmarker.NewCommit(),
 		reqCh:          make(chan *request, 1024),
 		checkPointer: &checkpoint.DiskCheckPointer{
 			Started: true,
@@ -43,7 +43,7 @@ func New(ts uint64, w wal.Wal) Scheduler {
 	}
 }
 
-func (s *TxnScheduler) Run() {
+func (s *TsoScheduler) Run() {
 	ticker := time.NewTicker(1 * time.Second)
 	for {
 		select {
@@ -58,10 +58,10 @@ func (s *TxnScheduler) Run() {
 	}
 }
 
-func (s *TxnScheduler) process(m *request) {
+func (s *TsoScheduler) process(m *request) {
 	switch m.typ {
 	case Start:
-		s.beginTsMarker.Add(m.ts)
+		s.beginTsWaiter.Add(m.ts)
 	case Done:
 		err := s.checkPointer.End(m.ts)
 		m.responseCh <- &response{err: err}
@@ -76,52 +76,52 @@ func (s *TxnScheduler) process(m *request) {
 
 		nextTs = atomic.AddUint64(&s.ts, 1)
 		for k, _ := range m.writeMap {
-			s.commitTsMarker.Add(k, nextTs)
+			s.commitTsWaiter.Add(k, nextTs)
 		}
 
-		if s.beginTsMarker.Del(m.ts) {
+		if s.beginTsWaiter.Del(m.ts) {
 			s.minTs = m.ts
 		}
 		m.responseCh <- &response{err, nextTs}
 	}
 }
 
-func (s *TxnScheduler) hasConflict(m *request) error {
+func (s *TsoScheduler) hasConflict(m *request) error {
 	if len(m.reads) == 0 {
 		return nil
 	}
 
 	for key, rts := range m.reads {
-		if commit, has := s.commitTsMarker.Get(key); has && commit.Ts > rts {
+		if commit, has := s.commitTsWaiter.Get(key); has && commit.Ts > rts {
 			return errmsg.TransactionConflict
 		}
 	}
 	return nil
 }
 
-func (s *TxnScheduler) gc() {
-	s.commitTsMarker.Del(s.minTs)
+func (s *TsoScheduler) gc() {
+	s.commitTsWaiter.Del(s.minTs)
 }
 
-func (s *TxnScheduler) Stop() {
+func (s *TsoScheduler) Stop() {
 	s.stopCh <- struct{}{}
 	<-s.stopCh
 }
 
-func (s *TxnScheduler) Begin() uint64 {
+func (s *TsoScheduler) Begin() uint64 {
 	ts := atomic.LoadUint64(&s.ts)
 	s.reqCh <- &request{typ: Start, ts: ts}
 	return ts
 }
 
-func (s *TxnScheduler) Commit(ts uint64, readCache map[string]uint64, writeCache map[string][]byte) (uint64, error) {
+func (s *TsoScheduler) Commit(ts uint64, readCache map[string]uint64, writeCache map[string][]byte) (uint64, error) {
 	responseCh := make(chan *response)
 	s.reqCh <- &request{Commit, ts, responseCh, readCache, writeCache}
 	resp := <-responseCh
 	return resp.ts, resp.err
 }
 
-func (s *TxnScheduler) Done(ts uint64) error {
+func (s *TsoScheduler) Done(ts uint64) error {
 	responseCh := make(chan *response)
 	s.reqCh <- &request{typ: Done, ts: ts, responseCh: responseCh}
 	resp := <-responseCh
