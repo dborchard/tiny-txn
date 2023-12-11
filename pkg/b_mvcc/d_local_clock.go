3567d7f8884fbf52c8314ecdbc83b10467a4bc0a
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-12-10 19:03:40 -0800

    Modify clock

--- pkg/b_mvcc/d_local_clock.go
+++ pkg/b_mvcc/d_local_clock.go
@@ -4,7 +4,7 @@ import "sync"
 
 type LocalClock struct {
 	*sync.Mutex
-	nextTs uint64
+	Ts uint64
 }
 
 var _ Clock = (*LocalClock)(nil)
@@ -12,8 +12,8 @@ var _ Clock = (*LocalClock)(nil)
 func (clk LocalClock) Now() uint64 {
 	defer func() {
 		clk.Lock()
-		clk.nextTs = clk.nextTs + 1
+		clk.Ts = clk.Ts + 1
 		clk.Unlock()
 	}()
-	return clk.nextTs
+	return clk.Ts
 }
